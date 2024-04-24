4c54e34d5b7ca72e96900d1e80eb1cb8a4c927b4
=Xuan Dong Ne <xuandong1405@gmail.com>
2024-04-24 14:43:48 +0700

    update

--- a.js
+++ a.js
@@ -97,3 +97,31 @@ function initengahan(){
   document.getElementById("lv3").onclick = function() {lv3.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
   document.getElementById("lv4").onclick = function() {lv4.style="opacity:0";slov+=1;this.onclick=null;checkslov();}
   function checkslov() {if(slov==4){kolombaru.style="position:relative;transform:scale(1)";fthilang();ftganti=1;setTimeout(ftmuncul,300);otomatis();setTimeout(aktipesan2,400);}}
+
+  document.getElementById("lv1").addEventListener("touchstart", function() {
+    lv1.style.opacity = "0";
+    slov += 1;
+    this.removeEventListener("touchstart", arguments.callee);
+    checkslov();
+});
+
+document.getElementById("lv2").addEventListener("touchstart", function() {
+    lv2.style.opacity = "0";
+    slov += 1;
+    this.removeEventListener("touchstart", arguments.callee);
+    checkslov();
+});
+
+document.getElementById("lv3").addEventListener("touchstart", function() {
+    lv3.style.opacity = "0";
+    slov += 1;
+    this.removeEventListener("touchstart", arguments.callee);
+    checkslov();
+});
+
+document.getElementById("lv4").addEventListener("touchstart", function() {
+    lv4.style.opacity = "0";
+    slov += 1;
+    this.removeEventListener("touchstart", arguments.callee);
+    checkslov();
+});
