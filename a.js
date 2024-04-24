a9f73f7500198624ccbc5b68400126c8b1bfed4a
=Xuan Dong Ne <xuandong1405@gmail.com>
2024-04-24 16:00:19 +0700

    update

--- a.js
+++ a.js
@@ -99,11 +99,12 @@ function aktipesan4() {
 var slov = 0;
 
 function handleInteraction(element) {
-  element.style.opacity = "0";
-  slov += 1;
-  element.removeEventListener("touchstart", handleInteraction);
-  element.removeEventListener("click", handleInteraction);
-  checkslov();
+  if (!element.handled) {
+    element.style.opacity = "0";
+    slov += 1;
+    element.handled = true;
+    checkslov();
+  }
 }
 
 document.getElementById("lv1").addEventListener("touchstart", function() {
@@ -137,6 +138,7 @@ document.getElementById("lv4").addEventListener("touchstart", function() {
 document.getElementById("lv4").addEventListener("click", function() {
   handleInteraction(this);
 });
+
 
 
 function checkslov() { if (slov == 4) { kolombaru.style = "position:relative;transform:scale(1)"; fthilang(); ftganti = 1; setTimeout(ftmuncul, 300); otomatis(); setTimeout(aktipesan2, 400); } }
